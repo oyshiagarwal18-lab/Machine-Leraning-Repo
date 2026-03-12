2cf74be392f7d352f5646a9bdb052a21104a5a4c
oyshiagarwal18-lab <oyshiagarwal18@gmail.com>
2026-03-12 09:37:31 +0530

    added supervised learning program

---
+++ supervised learning.py
@@ -0,0 +1,18 @@
+# Import libraries
+import numpy as np
+from sklearn.linear_model import LinearRegression
+
+# Training data
+X = np.array([[1], [2], [3], [4], [5]])   # input
+y = np.array([2, 4, 6, 8, 10])            # output
+
+# Create model
+model = LinearRegression()
+
+# Train model
+model.fit(X, y)
+
+# Predict
+prediction = model.predict([[6]])
+
+print("Prediction:", prediction)
